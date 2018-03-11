5240bfcd5d832c74aa750f4a83e8967032046bf2
Brooke Mortensen <brookeamymo@gmail.com>
2018-03-10 18:46:37 -0700

    initialized firebase

---
+++ assets/logic.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBOK2twpeuoZA2oeJBfiI-EOGaJrvI0ck4",
+        authDomain: "fir-trainscheduler.firebaseapp.com",
+        databaseURL: "https://fir-trainscheduler.firebaseio.com",
+        projectId: "fir-trainscheduler",
+        storageBucket: "fir-trainscheduler.appspot.com",
+        messagingSenderId: "964966023007"
+    };
+  
+  firebase.initializeApp(config);
